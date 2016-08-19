4c99c73713973f9a5639276900f15eb8c0f73ad4
jingyi <1849612320@qq.com>
2016-08-19 16:30:43 +0800

    Initial Commit

---
+++ ocday01/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ocday01
+//
+//  Created by 691 on 16/8/19.
+//  Copyright © 2016年 691. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
